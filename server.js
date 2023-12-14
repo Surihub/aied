7f4ccc8f23b97b4401a80f73e29cd9945b2a2495
Subhin Hwang <sbhath17@gmail.com>
2023-12-14 15:53:54 +0900

    aied init(copied)

---
+++ server.js
@@ -0,0 +1,27 @@
+// 
+const express = require('express');
+// 객체를 만들기
+const app = express();
+
+app.listen(8080, function(){
+    console.log('포트 8080에서 실행중입니다....')
+
+});
+
+app.get('/', function(요청, 응답){
+    응답.sendFile(__dirname+"/index.html")
+})
+
+app.get('/math', function(요청, 응답){
+    응답.sendFile(__dirname+"/math.html")
+})
+
+app.get('/XR', function(요청, 응답){
+    응답.sendFile(__dirname+"/XR.html")
+})
+
+// 누군가가 /eda로 방문을 하면 관련 안내문을 띄워주자
+app.get('/EDA', function(요청, 응답){
+    응답.sendFile(__dirname+"/data.html")
+})
+
